3d852a9bd0889ec39b0fb8962167c5efb4ac4b23
Michelle Barker <barkerwebdev47@gmail.com>
2020-07-18 02:08:32 -0700

    finalized shopping cart

--- apps/app1/views.py
+++ apps/app1/views.py
@@ -12,9 +12,31 @@ def show_one(request):
     return render(request, "show_one.html", context)
 
 def shop_cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {
+        'items':items,
+        'order':order,
+    }
     return render(request, "shop_cart.html", context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {
+        'items':items,
+        'order':order,
+    }
     return render(request, "checkout.html", context)
